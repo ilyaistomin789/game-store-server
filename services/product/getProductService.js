642547a6cae692232b384ebf076339860fe012f2
Ilya.Istomin <Ilya.Istomin@itechart-group.com>
2021-09-08 17:11:15 +0300

    Make things typed

--- services/product/getProductService.js
+++
@@ -1,6 +0,0 @@
-const Product = require('../../models/product');
-
-module.exports = async (req, res) => {
-    const products = await Product.find({});
-    products !== null ? res.json(products) : res.sendStatus(404);
-}
